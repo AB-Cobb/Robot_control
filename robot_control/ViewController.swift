b912601d662bf17a072654fd2aafce8fb42b3191
laptop <oaclc@clc330-013ml.local>
2020-02-27 14:13:19 -0500

    Initial Commit

---
+++ robot_control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  robot_control
+//
+//  Created by laptop on 2020-02-27.
+//  Copyright © 2020 joey_corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
